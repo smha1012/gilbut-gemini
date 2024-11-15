88f22766fb53940109b1f1a97c8efb332ad39446
smha1012 <smha1012@github.com>
2024-11-15 09:37:21 +0900

    image_qa 코드 수정

--- 5_gemini_api_android/2_multimodal/1_image_qa/MainActivity.kt
+++ 5_gemini_api_android/2_multimodal/1_image_qa/MainActivity.kt
@@ -1,30 +1,39 @@
-package com.example.helloworld
+package com.example.myapplication
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.lifecycleScope
-import com.example.helloworld.ui.theme.HelloWorldTheme
+import com.example.myapplication.ui.theme.MyApplicationTheme
+import android.graphics.BitmapFactory
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.lifecycle.lifecycleScope
+import com.google.ai.client.generativeai.type.content
 import com.google.ai.client.generativeai.GenerativeModel
 import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContent {
-            HelloWorldTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+            MyApplicationTheme {
+                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
                 }
             }
         }
@@ -67,7 +76,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    HelloWorldTheme {
+    MyApplicationTheme {
         Greeting("Android")
     }
 }
